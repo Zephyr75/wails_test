aa7f8f6bcba123693dffd09893285f4438fa2c1f
Antoine <antoine.familleroger@gmail.com>
2023-04-10 23:11:53 +0200

    Update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,4 +12,8 @@ import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
 library.add(faCaretDown)
 library.add(faCaretUp)
 
+// new Vue({
+//     render: h => h(App),
+// }).$mount('#app').component('font-awesome-icon', FontAwesomeIcon)
+
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
